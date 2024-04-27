e80f52d41b1f7a260065c0403b6f80e454aa5e95
An Jaebeom <ajb8533296@gmail.com>
2024-04-27 21:47:33 +0900

    first commit

---
+++ js/map.js
@@ -0,0 +1,206 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.56646, 126.98121), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+        mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
+    };
+
+// 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places();
+
+
+// 내 위치 마커 START
+function locationLoadSuccess(pos) {
+    // 현재 위치 받아오기
+    var currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+
+    // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
+    map.panTo(currentPos);
+
+    var imageSrc = './icons/mylocation.svg', // 마커이미지의 주소입니다
+    imageSize = new kakao.maps.Size(128, 128), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude); // 마커가 표시될 위치입니다
+    
+    // 마커 생성
+    var marker = new kakao.maps.Marker({
+        position: markerPosition,
+        image: markerImage // 마커이미지 설정 
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(null);
+    marker.setMap(map);
+};
+
+function locationLoadError(pos) {
+    alert('위치 정보를 가져오는데 실패했습니다.');
+};
+
+// 위치 가져오기 버튼 클릭시
+function getCurrentPosBtn() {
+    navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
+};
+// 내 위치 마커 END
+
+
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+let default_location = {
+    "seoul": new kakao.maps.LatLng(37.5659436, 126.9778288),
+    "incheon": new kakao.maps.LatLng(37.4560939, 126.703727),
+    "gwangju": new kakao.maps.LatLng(35.1587263, 126.8497696),
+    "ulsan": new kakao.maps.LatLng(35.5390653, 129.3103105),
+    "busan": new kakao.maps.LatLng(35.1793999, 129.0739666),
+    "daegu": new kakao.maps.LatLng(35.8712121, 128.6010165),
+    "gangwon": new kakao.maps.LatLng(37.8306817, 127.6797105),
+    "gyeonggi": new kakao.maps.LatLng(37.2749107, 127.0070014),
+    "gyeongsangnam": new kakao.maps.LatLng(35.2382949, 128.6902093),
+    "gyeongsangbuk": new kakao.maps.LatLng(36.5773712, 128.503755),
+    "jeonnam": new kakao.maps.LatLng(34.6959784, 125.9375888),
+    "jeonbuk": new kakao.maps.LatLng(35.8160554, 127.111812),
+    "chungnam": new kakao.maps.LatLng(36.4937087, 126.9066248),
+    "chungbuk": new kakao.maps.LatLng(36.6358136, 127.4891451),
+    "jeju": new kakao.maps.LatLng(33.4889923, 126.4973843),
+    "daejeon": new kakao.maps.LatLng(36.3490194, 127.3842079),
+    "sejong": new kakao.maps.LatLng(36.4779772, 127.2864818)
+};
+
+function set_location(location) {
+    map.panTo(default_location[location]);
+}
+ 
+// 지도 레벨은 지도의 확대 수준을 의미합니다
+// 지도 레벨은 1부터 14레벨이 있으며 숫자가 작을수록 지도 확대 수준이 높습니다
+function zoomIn() {        
+    // 현재 지도의 레벨을 얻어옵니다
+    var level = map.getLevel();
+
+    // 지도를 1레벨 내립니다 (지도가 확대됩니다)
+    map.setLevel(level - 1);
+}    
+
+function zoomOut() {
+    // 현재 지도의 레벨을 얻어옵니다
+    var level = map.getLevel();
+
+    // 지도를 1레벨 올립니다 (지도가 축소됩니다)
+    map.setLevel(level + 1);
+}
+
+// 키워드 검색을 요청하는 함수입니다
+function searchPlaces() {
+
+    var keyword = document.getElementsByClassName('search')[0].value;
+
+    if (!keyword.replace(/^\s+|\s+$/g, '')) {
+        alert('키워드를 입력해주세요!');
+        return false;
+    }
+
+    // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
+    ps.keywordSearch(keyword, function (data, status) {
+        if (status === kakao.maps.services.Status.OK) {
+
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+            // LatLngBounds 객체에 좌표를 추가합니다
+            var bounds = new kakao.maps.LatLngBounds();
+
+            for (var i = 0; i < data.length; i++) {
+                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+            }
+
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+            map.setBounds(bounds);
+        }
+    });
+
+}
+
+// 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
+function setMapType() {
+    const satelite_grey = document.getElementById('satelite_grey');
+    const satelite_selected = document.getElementById('satelite_selected');
+    if (satelite_grey.style.display === 'none') {
+        // 지도뷰
+        satelite_grey.style.display = 'block';
+        satelite_selected.style.display = 'none';
+        map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+    } else {
+        // 위성뷰
+        satelite_grey.style.display = 'none';
+        satelite_selected.style.display = 'block';
+        map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+    }
+}
+
+// 급식소 핀
+
+let positions = new Array();
+
+for (let cafeteria of json_data) {
+    try {
+        positions.push(
+            {
+                fcltyNm: cafeteria.fcltyNm._text,
+                rdnmadr: cafeteria.rdnmadr._text,
+                mlsvTrget: cafeteria.mlsvTrget._text,
+                mlsvTime: cafeteria.mlsvTime._text,
+                mlsvDate: cafeteria.mlsvDate._text,
+                phoneNumber: cafeteria.phoneNumber._text,
+                latlng: new kakao.maps.LatLng(Number(cafeteria.latitude._text), Number(cafeteria.longitude._text))
+            }
+        );
+    } catch {}
+};
+
+// 마커 이미지의 이미지 주소입니다
+var imageSrc = "icons/pin_notenadbled.svg";
+var cafeteria_list_box = document.getElementById('cafeteria_list_box')
+for (var i = 0; i < positions.length; i++) {
+
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(128, 128);
+
+    // 마커 이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+    // 마커를 생성합니다
+    displayMarker(positions[i]);
+}
+
+function displayMarker(positions) {
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: positions.latlng, // 마커를 표시할 위치
+        // title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage, // 마커 이미지
+        // clickable: true
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        document.getElementsByClassName('cafeteria_list_box')[0].style.display = "block";
+
+        document.getElementsByClassName('cafeteria_name')[0].innerText = positions.fcltyNm;
+        document.getElementsByClassName('cafeteria_address')[0].innerText = positions.rdnmadr;
+        document.getElementsByClassName('target_person')[0].innerText = positions.mlsvTrget;
+        document.getElementsByClassName('time')[0].innerText = positions.mlsvTime;
+        document.getElementsByClassName('day_of_the_week')[0].innerText = positions.mlsvDate;
+        document.getElementsByClassName('call')[0].href = "tel:" + positions.phoneNumber;
+        if (positions.phoneNumber == undefined) {
+            alert('해당 급식소의 전화번호가 없습니다.');
+        }
+    });
+}
+
+function close_cafeteria_list_box() {
+    document.getElementsByClassName('cafeteria_list_box')[0].style.display = "none";
+}
