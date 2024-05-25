d9a60f0ca0b870ff83cb8b7fd025a51186dc0a86
An Jaebeom <ajb8533296@gmail.com>
2024-05-25 14:56:50 +0900

    chore: Update list.js to use deferred loading for improved page performance

--- js/list.js
+++ js/list.js
@@ -1,7 +1,7 @@
-let lists = document.createElement("div")
-const obj = document.getElementsByClassName("safe_zone_list")[0];
 
 function display_shelter() {
+    const safe_zone_list = document.getElementById("safe_zone_list");
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             const latitude = position.coords.latitude;
@@ -9,13 +9,11 @@ function display_shelter() {
 
             axios.get(domain + near_shelter_api + "?latitude=" + latitude + "&longitude=" + longitude).then((response) => {
                 if (response.data.length === 0) {
-                    lists.innerHTML = '<h1 class="text-[#6F6864] mt-10">회원님의 근처엔<br>긴급대피소가 없네요 :(</h1>'
-                    obj.appendChild(lists);
+                    safe_zone_list.innerHTML = '<h1 class="text-[#6F6864] mt-10">회원님의 근처엔<br>긴급대피소가 없네요 :(</h1>';
                 } else {
                     let phone;
+                    let shelter_list_html = '';
                     for (let shelter of response.data) {
-                        let lists = document.createElement("div")
-
                         if (shelter.mngpsTelno != "") {
                             phone = '<a href="tel:' + shelter.mngpsTelno + '">'+
                             '            <div class="flex justify-center items-center w-[50px] h-[50px] bg-[#66cc54] rounded-[14px] cursor-pointer hover:bg-[#148300]">'+
@@ -26,7 +24,7 @@ function display_shelter() {
                             phone = ''
                         }
 
-                        lists.innerHTML = '<a href="/?id=' + shelter.id + '">'+
+                        shelter_list_html += '<a href="/?id=' + shelter.id + '">'+
                             '    <div class="bg-[#e6e6e6] rounded-[15px] px-6 py-3 mb-2">'+
                             '        <p class="text-[25px]">' + shelter.vtAcmdfcltyNm + '</p>'+
                             '        <p class="text-[15px] text-[#707070]">' + shelter.dtlAdres + '</p>'+
@@ -43,13 +41,12 @@ function display_shelter() {
                             '        </div>'+
                             '    </div>'
                             '</a>'
-                        obj.appendChild(lists);
-                    }
+                        }
+                    safe_zone_list.innerHTML = shelter_list_html;
                 }
             });
         });
     } else {
-        lists.innerHTML = '<h1 class="text-[#6F6864] mt-10">GPS가 차단되었거나<br>찾을 수 없어요. :(</h1>'
-        obj.appendChild(lists);
+        safe_zone_list.innerHTML = '<h1 class="text-[#6F6864] mt-10">GPS가 차단되었거나<br>찾을 수 없어요. :(</h1>';
     }
 }
