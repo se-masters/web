60a292784fb0401c71459c5901b68bb93be6cf6b
An Jaebeom <ajb8533296@gmail.com>
2024-05-27 19:34:35 +0900

    add earthquake pin, add readme.md

--- js/variable.js
+++ js/variable.js
@@ -1,9 +1,11 @@
-const domain = window.location.origin + "/api/"
-// const domain = "http://localhost:9000/"
+// const domain = window.location.origin + "/api/"
+const domain = "http://localhost:9000/"
 
 const shelter_api = "shelter/findAll"
 const interim_housing_api = "interimHousing/findAll"
 
 const near_shelter_api = "shelter/findNear"
+
+const earthquake_api = "earthquakeInfo"
 
 axios.defaults.baseURL = window.location.origin;
